25c5c8d84619f782378578495410972436d5d3c8
RisaGithub <119109722+RisaGithub@users.noreply.github.com>
2025-03-02 06:17:33 -0800

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,3 +1,4 @@
-function showAlert() {
-    alert("Button clicked!");
+function changeColor() {
+    let btn = document.getElementById("myButton");
+    btn.style.backgroundColor = "red"; // Change to any color you want
 }
